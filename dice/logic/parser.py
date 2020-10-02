903cfd6f49126d11c184b9c429ffe1133fa153fa
Grey Leman <dgreyleman@gmail.com>
2020-10-01 21:47:51 -0600

    Fixed issue where stored minus values would not evaluate properly

--- dice/logic/parser.py
+++ dice/logic/parser.py
@@ -7,25 +7,27 @@ class BaseComponent:
 
 class RollComponent(BaseComponent):
     
-    def __init__(self):
-        self.count = "" 
-        self.die = ""
+    def __init__(self, count, die, minus):
+        self.count = count 
+        self.die = die
+        self.minus = minus
 
     def evaluate(self):
         total = 0
         rolls = []
         countInt = int(self.count)
         dieInt = int(self.die)
+        minusMod = -1 if self.minus else 1
         for i in range(0, countInt):
-            roll = randint(1, dieInt)
+            roll = randint(1, dieInt) * minusMod
             total += roll
             rolls.append(roll)
         return total, rolls
 
 class ConstantComponent(BaseComponent):
 
-    def __init__(self):
-        self.count = ""
+    def __init__(self, count):
+        self.count = count
 
     def evaluate(self):
         countInt = int(self.count)
@@ -33,62 +35,52 @@ class ConstantComponent(BaseComponent):
 
 SUB_PLUS = r"\s*\+\s*"
 SUB_MINUS = r"\s*\-\s*"
-SEPARATE_COMPONENTS = r"[^\+\-]+|\+|\-"
-SIMPLE_COMPONENT_MATCH = r"\b([0-9]+d[0-9]+|[0-9]+)\b" 
-COMPLEX_COMPONENT_MATCH = r"([0-9]+d[0-9]+|[0-9]+|\+|\-)"
+REPEAT_PLUS_MATCH = r"\+\++"
+SEPARATE_COMPONENTS = r"[^\+]+|\+"
+SIMPLE_COMPONENT_MATCH = r"^([0-9]+d[0-9]+|[0-9]+)$" 
 
-def parseSingle(rollString) -> "BaseComponent":
-    rollString = rollString.lower().replace(" ", "")
-    if "d" in rollString:
-        roll = RollComponent()
-    else:
-        roll = ConstantComponent()
-    buildCount = True
-    for c in rollString:
-        if c == "d":
-            buildCount = False
-            continue
-        if buildCount:
-            roll.count += c
-        else:
-            roll.die += c
-    return roll
+def parseSingle(rollString, minus):
+    if rollString.isdigit():
+        minusMod = "-" if minus else ""
+        return ConstantComponent(minusMod + rollString)
+    rollComponents = rollString.lower().split("d")
+    if len(rollComponents) != 2 or not (rollComponents[0].isdigit() and rollComponents[1].isdigit()):
+        raise ValueError("Invalid roll string")
+    return RollComponent(rollComponents[0], rollComponents[1], minus)
 
-def mapNameToBaseRolls(rollString, mapNameToRoll):
+def splitRollString(rollString):
     components = re.sub(SUB_PLUS, "+", rollString)
     components = re.sub(SUB_MINUS, "-", components)
-    components = re.findall(SEPARATE_COMPONENTS, components)
-    decipheredComponents = []
-    for component in components:
-        if component == "+" or component == "-" or re.fullmatch(SIMPLE_COMPONENT_MATCH, component):
-            decipheredComponents += [component]
-        else:
-            decipheredComponents += mapNameToBaseRolls(mapNameToRoll(component), mapNameToRoll)
-    return decipheredComponents
+    components = components.replace("--", "+")
+    components = components.replace("-", "+-")
+    components = re.sub(REPEAT_PLUS_MATCH, "+", components)
+    return re.findall(SEPARATE_COMPONENTS, components)
 
-def parse(rollString, mapNameToRoll): 
-    rolls = []
-    ops = []
-    components = mapNameToBaseRolls(rollString, mapNameToRoll)
+def mapRollNameToBaseRolls(rollString, invert, rollStack, rollLookup):
+    components = splitRollString(rollString)
+    mappedComponents = []
     for component in components:
-        if component == "+" or component == "-":
-            ops.append(component)
+        if component == "+":
+            continue
+        minus = component.startswith("-")
+        component = component[1:] if minus else component
+        if re.match(SIMPLE_COMPONENT_MATCH, component) is not None:
+            mappedComponents.append(parseSingle(component, invert != minus))
         else:
-            rolls.append(parseSingle(component))
-    if len(rolls) != len(ops) + 1:
-        print(len(rolls) + " " + len(ops))
-        raise ValueError("Wrong number of args")
-    first = rolls.pop(0)
-    total, individuals = first.evaluate()
-    for i in range(0, len(rolls)):
-        op = ops[i]
-        roll = rolls[i]
+            resolvedName = rollLookup(component)
+            if resolvedName in rollStack:
+                raise ValueError("Roll cannot be defined by itself") 
+            c = mapRollNameToBaseRolls(resolvedName, invert != minus, rollStack + [resolvedName], rollLookup)
+            mappedComponents.extend(c)
+    return mappedComponents
+
+def parse(rollString, rollLookup): 
+    rolls = mapRollNameToBaseRolls(rollString, False, [], rollLookup)
+    total = 0
+    individuals = []
+    for roll in rolls:
         nextRoll, nextIndividuals = roll.evaluate()
-        if op == "+":
-            total += nextRoll
-            individuals.extend(nextIndividuals)
-        else:
-            total -= nextRoll
-            nextIndividuals = map(lambda r : r * -1, nextIndividuals)
-            individuals.extend(nextIndividuals)
-    return total, individuals 
+        total += nextRoll
+        individuals.extend(nextIndividuals)
+    return total, individuals
+
